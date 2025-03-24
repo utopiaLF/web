fa31b8630ad41071f08e90e764ec7982803bab2b
Johnny <jahongirhakimov677@gmail.com>
2025-03-24 22:31:21 +0500

    some changes and updates

---
+++ app.js
@@ -0,0 +1,6 @@
+const menuButton = document.querySelector('.burger');
+const barList = document.getElementById('barList')
+
+menuButton.addEventListener('click', ()=>{
+    barList.classList.toggle('active');
+})
