1083fe35f7da700cefeef106b111e601ff28de7f
Johnny <jahongirhakimov677@gmail.com>
2025-04-11 00:45:09 +0500

    added loading animation

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ const barList = document.getElementById('barList')
 // menuButton.addEventListener('click', ()=>{
 //     barList.classList.toggle('active');
 // })
+
+window.onload = function () {
+    const box = document.getElementById('loading');
+    box.classList.add('off');
+}
